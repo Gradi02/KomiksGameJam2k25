16cef558b117b1f9818a98032709c3080821c15e
Adrian Nowowiejski <anowowiejski123@gmail.com>
2025-04-26 04:47:12 +0200

    interactions

---
+++ Assets/Scripts/Interactions/InterHelp.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class InterHelp : EnvSignInteractionAction
+{
+    public override void Play()
+    {
+        Debug.Log("help");
+    }
+}
---
+++ Assets/Scripts/Interactions/InterStartGame.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class InterStartGame : EnvSignInteractionAction
+{
+    public override void Play()
+    {
+        Debug.Log("start");
+    }
+}
