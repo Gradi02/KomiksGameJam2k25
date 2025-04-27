42f41d3d71d1338ffa637e7875209cc301b01307
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2025-04-27 02:52:19 +0200

    .

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -27,23 +27,12 @@ public class PlayerAttack : MonoBehaviour
                 if(player.GetComponent<PlayerMovement>().IsGrounded()){
                     // Vector2 snakeDirection = player.transform.right;
                     Vector3 snakeGroundPosition = player.transform.position;
-                    snakeGroundPosition.y -= 1;
+                    snakeGroundPosition.y -= 0.8f;
                     Instantiate(snakeGroundPrefab, snakeGroundPosition, Quaternion.Euler(0, 0, 0));
                     //snakeGround.GetComponent<SnakeGround>().Initialize(snakeDirection);
 
                     timeBtwAttack = startTimeBtwAttack;
 
-                } else {
-                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    Vector2 direction = (mousePos - firePoint.position).normalized;
-
-                    float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-                    Quaternion rotation = Quaternion.Euler(0, 0, angle - 90);
-
-                    GameObject snake = Instantiate(snakePrefab, firePoint.position, rotation);
-                    snake.GetComponent<Rigidbody2D>().linearVelocity = direction * snakeSpeed;
-                    timeBtwAttack = startTimeBtwAttack;
                 }
 
             }
