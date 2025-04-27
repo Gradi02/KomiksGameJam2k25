dc62617d45fa23960aff13f849b46dbd50e6b44d
Tobkubos <128313959+Tobkubos@users.noreply.github.com>
2025-04-27 03:36:41 +0200

    .

---
+++ Assets/Scripts/KameHameHa.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class KameHameHa : MonoBehaviour
+{
+    public Light2D GlobalLight;
+    IEnumerator KameHameHaCoroutine()
+    {
+        LeanTween.value(1, 15, 4f)
+            .setOnUpdate((float val) => {
+                GlobalLight.intensity = val;
+            })
+            .setEase(LeanTweenType.easeInOutSine); // 1111111111111111111
+
+        LeanTween.value(1, 15, 5f)
+            .setOnUpdate((float val) => {
+                CinemachineShake.Instance.ShakeCamera(val, .1f);
+            })
+            .setEase(LeanTweenType.easeInOutSine); //22222222222222222222
+        
+        yield return new WaitForSeconds(4.1f);
+
+        //TUTAJ ROZPIERDOL WSZYSTKICH PRZECIWNIKÓW
+        // I WYLACZ SKRYPTY MOVEMENT I COLLISION NA GRACZU
+
+
+
+
+            LeanTween.value(15, 5000, 1f)
+        .setOnUpdate((float val) => {
+            GlobalLight.intensity = val;
+        })
+        .setEase(LeanTweenType.easeInOutSine); // 1111111111111111111
+        yield return new WaitForSeconds(1.1f);
+            LeanTween.value(5000, 1, 1f)
+       .setOnUpdate((float val) => {
+           GlobalLight.intensity = val;
+       })
+       .setEase(LeanTweenType.easeInOutSine); // 1111111111111111111
+
+        LeanTween.value(15, 1, 2.1f)
+            .setOnUpdate((float val) => {
+                CinemachineShake.Instance.ShakeCamera(val, .1f);
+            })
+            .setEase(LeanTweenType.easeInOutSine); //22222222222222222222
+        yield return new WaitForSeconds(2.2f);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(KameHameHaCoroutine());
+        }
+    }
+}
